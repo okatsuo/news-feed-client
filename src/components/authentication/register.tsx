c047363200b44c59b5df620a5aa0fee366d15cab
okatsuo <rafael.goncanna@gmail.com>
2021-12-19 00:30:33 -0300

    refactor: move register folder to correct place

---
+++ src/components/authentication/register.tsx
@@ -0,0 +1,83 @@
+import { EmailOutlined, Facebook, GitHub, Google, LockOutlined, PersonOutline } from '@mui/icons-material'
+import { Box, Button, IconButton, InputAdornment, TextField, Tooltip, Typography } from '@mui/material'
+
+export const Register = () => {
+  return (
+    <Box sx={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column'
+    }}>
+      <Typography variant='h3' color='primary'>
+        Criar conta
+      </Typography>
+
+      <Box>
+        <IconButton>
+          <Tooltip title='Entrar com o Facebook (não ativado ainda)'>
+            <Facebook />
+          </Tooltip>
+        </IconButton>
+        <IconButton>
+          <Tooltip title='Entrar com o Google (não ativado ainda)'>
+            <Google />
+          </Tooltip>
+        </IconButton>
+        <IconButton>
+          <Tooltip title='Entrar com o GitHUB (não ativado ainda)'>
+            <GitHub />
+          </Tooltip>
+        </IconButton>
+      </Box>
+      <Typography variant='body2'>ou inscreva-se com teu email</Typography>
+      <form>
+        <Box sx={{ display: 'flex', flexDirection: 'column', width: '300px' }}>
+          <TextField
+            placeholder='Nome'
+            type='text'
+            margin='normal'
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position='start'>
+                  <PersonOutline />
+                </InputAdornment>
+              )
+            }}
+          />
+
+          <TextField
+            placeholder='Email'
+            type='email'
+            margin='normal'
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position='start'>
+                  <EmailOutlined />
+                </InputAdornment>
+              )
+            }}
+          />
+
+          <TextField
+            placeholder='Senha'
+            type='password'
+            margin='normal'
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position='start'>
+                  <LockOutlined />
+                </InputAdornment>
+              ),
+            }}
+          />
+
+          <Button variant='contained'>
+            REGISTRAR
+          </Button>
+        </Box>
+      </form>
+    </Box>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,86 +1,8 @@
-import { AppBar, Box, Button, IconButton, InputAdornment, TextField, Toolbar, Typography } from '@mui/material'
-import { EmailOutlined, Facebook, GitHub, Google, LockOutlined, PersonOutline } from '@mui/icons-material'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position='static'>
-        <Toolbar>
-          <Typography>Hello!</Typography>
-        </Toolbar>
-      </AppBar>
-      <Box sx={{
-        paddingTop: ({ spacing }) => spacing(4),
-        display: 'flex',
-        justifyContent: 'flex',
-        alignItems: 'center',
-        flexDirection: 'column'
-      }}>
-        <Typography variant='h3' color='primary'>
-          Criar conta
-        </Typography>
-
-        <Box>
-          <IconButton>
-            <Facebook />
-          </IconButton>
-          <IconButton>
-            <Google />
-          </IconButton>
-          <IconButton>
-            <GitHub />
-          </IconButton>
-        </Box>
-        <Typography variant='body2'>ou inscreva-se com teu email</Typography>
-        <form>
-          <Box sx={{ display: 'flex', flexDirection: 'column', width: '300px' }}>
-            <TextField
-              placeholder='Nome'
-              type='text'
-              margin='normal'
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position='start'>
-                    <PersonOutline />
-                  </InputAdornment>
-                )
-              }}
-            />
-
-            <TextField
-              placeholder='Email'
-              type='email'
-              margin='normal'
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position='start'>
-                    <EmailOutlined />
-                  </InputAdornment>
-                )
-              }}
-            />
-
-            <TextField
-              placeholder='Senha'
-              type='password'
-              margin='normal'
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position='start'>
-                    <LockOutlined />
-                  </InputAdornment>
-                ),
-              }}
-            />
-
-            <Button variant='contained'>
-              REGISTRAR
-            </Button>
-          </Box>
-        </form>
-      </Box>
-    </Box>
+    <div>Home page ?</div>
   )
 }
 
