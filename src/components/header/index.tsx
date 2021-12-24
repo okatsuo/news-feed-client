d0af9c60a8cabf94a81c099dd9e29b8b811fc98f
okatsuo <rafael.goncanna@gmail.com>
2021-12-24 20:48:04 -0300

    fix: imports

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,17 @@
 import { Logout, PersonOutlined } from '@mui/icons-material'
-import { AppBar, Avatar, Box, Container, IconButton, ListItemIcon, Menu, MenuItem, Toolbar, Tooltip, Typography } from '@mui/material'
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Container,
+  IconButton,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from '@mui/material'
 import Router from 'next/router'
 import { useContext, useState } from 'react'
 import { AuthenticationContext } from '../../context/authentication'
@@ -7,7 +19,6 @@ import { AppRoutes } from '../../utils/appRoutes'
 
 export const Navbar = () => {
   const { loggedUser, logout } = useContext(AuthenticationContext)
-
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -29,7 +40,7 @@ export const Navbar = () => {
                 <Typography>News Feed</Typography>
               </Tooltip>
             </Box>
-            <Box textAlign='right' display={'flex'} alignItems={'center'}>
+            <Box textAlign='right' display={'flex'} alignItems={'center'} justifyContent={'flex-end'}>
               <Box mr={0.5}>
                 <Typography>{loggedUser?.name}</Typography>
                 <Typography variant='subtitle2'>{loggedUser?.email}</Typography>
