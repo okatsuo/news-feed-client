cd8c0f1dd2a49ad749f6c3a7cfba2a569d410511
okatsuo <rafael.goncanna@gmail.com>
2021-12-24 21:09:30 -0300

    feat: button to return to home page from my-profile

--- src/components/my-profile/index.tsx
+++ src/components/my-profile/index.tsx
@@ -1,5 +1,5 @@
-import { AddAPhotoOutlined } from '@mui/icons-material'
-import { Avatar, Badge, Box, Button, IconButton, TextField, Typography } from '@mui/material'
+import { AddAPhotoOutlined, ArrowBack } from '@mui/icons-material'
+import { Avatar, Badge, Box, Button, IconButton, TextField, Tooltip, Typography } from '@mui/material'
 import { Form, Formik } from 'formik'
 import { useContext, useState } from 'react'
 import { AuthenticationContext } from '../../context/authentication'
@@ -11,6 +11,7 @@ import Router from 'next/router'
 import { User } from '../../graphql/types/user'
 import { ScreenAlert } from '../alert'
 import { LoadingButton } from '@mui/lab'
+import { AppRoutes } from '../../utils/appRoutes'
 
 const schema = Yup.object().shape({
   name: Yup.string().required('Nome é necessário')
@@ -72,85 +73,104 @@ export const MyProfile = () => {
   return (
     <Box display='flex' justifyContent='center' p={5}>
       <Box sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
         width: '250px'
       }}>
-        <Badge
-          overlap='circular'
-          sx={{ mb: ({ spacing }) => spacing(3) }}
-          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-          badgeContent={
-            <label>
-              <input
-                style={{ display: 'none' }}
-                onChange={({ target }) => {
-                  target.files && handleImageChange(target.files)
-                }}
-                type='file'
-                accept='image/x-png, image/jpeg'
-              />
-              <IconButton
-                component='span'
-                sx={{
-                  backgroundColor: ({ palette }) => palette.primary.main,
-                  transition: '0.2s',
-                  ':hover': {
-                    backgroundColor: ({ palette }) => palette.primary.main, opacity: 0.9
-                  }
-                }}
-              >
-                <AddAPhotoOutlined />
-              </IconButton>
-            </label>
-          }
+        <Box>
+          <IconButton onClick={() => Router.push(AppRoutes.home)}>
+            <Tooltip title='Voltar para página principal'>
+              <ArrowBack />
+            </Tooltip>
+          </IconButton>
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'start',
+            alignItems: 'center',
+          }}
         >
-          <Avatar
-            sx={{
-              width: '150px',
-              height: '150px',
-            }}
-            src={userPicture?.pictureUrl || loggedUser.picture || ''}
-          />
-        </Badge>
-        <Box textAlign={'center'}>
-          <Typography
-            variant='caption'
-          >
-            Perfil criado em: {formatDate(loggedUser.created_at)}
-          </Typography>
-          <Formik
-            initialValues={initialValues}
-            onSubmit={handleSubmit}
-            validationSchema={schema}
-          >
-            {({ errors, handleChange }) => (
-              <Form>
-                <TextField
-                  type='text'
-                  label='Nome'
-                  margin='normal'
-                  name='name'
-                  defaultValue={initialValues.name}
-                  onChange={handleChange}
-                  helperText={errors.name}
-                  fullWidth
+          <Badge
+            overlap='circular'
+            sx={{ mb: ({ spacing }) => spacing(3) }}
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+            badgeContent={
+              <label>
+                <input
+                  style={{ display: 'none' }}
+                  onChange={({ target }) => {
+                    target.files && handleImageChange(target.files)
+                  }}
+                  type='file'
+                  accept='image/x-png, image/jpeg'
                 />
-                <Box display='flex' justifyContent='space-evenly' mt={1}>
-                  <Button color='error' variant='contained' onClick={handleCancel}>Cancelar</Button>
-                  <LoadingButton
-                    loading={loading}
-                    color='primary'
-                    variant='contained'
-                    type='submit'
-                  >
-                    Salvar
-                  </LoadingButton>
-                </Box>
-              </Form>
-            )}
-          </Formik>
+                <IconButton
+                  component='span'
+                  sx={{
+                    backgroundColor: ({ palette }) => palette.primary.main,
+                    transition: '0.2s',
+                    ':hover': {
+                      backgroundColor: ({ palette }) => palette.primary.main, opacity: 0.9
+                    }
+                  }}
+                >
+                  <AddAPhotoOutlined />
+                </IconButton>
+              </label>
+            }
+          >
+            <Avatar
+              sx={{
+                width: '150px',
+                height: '150px',
+              }}
+              src={userPicture?.pictureUrl || loggedUser.picture || ''}
+            />
+          </Badge>
+          <Box textAlign={'center'}>
+            <Typography
+              variant='caption'
+            >
+              Perfil criado em: {formatDate(loggedUser.created_at)}
+            </Typography>
+            <Formik
+              initialValues={initialValues}
+              onSubmit={handleSubmit}
+              validationSchema={schema}
+            >
+              {({ errors, handleChange }) => (
+                <Form>
+                  <TextField
+                    defaultValue={loggedUser.email}
+                    disabled
+                    fullWidth
+                  />
+
+                  <TextField
+                    type='text'
+                    label='Nome'
+                    margin='normal'
+                    name='name'
+                    defaultValue={initialValues.name}
+                    onChange={handleChange}
+                    helperText={errors.name}
+                    fullWidth
+                  />
+                  <Box display='flex' justifyContent='space-evenly' mt={1}>
+                    <Button color='error' variant='contained' onClick={handleCancel}>Cancelar</Button>
+                    <LoadingButton
+                      loading={loading}
+                      color='primary'
+                      variant='contained'
+                      type='submit'
+                    >
+                      Salvar
+                    </LoadingButton>
+                  </Box>
+                </Form>
+              )}
+            </Formik>
+          </Box>
         </Box>
       </Box>
       <ScreenAlert
